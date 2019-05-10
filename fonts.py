e60245a17cebfa0fad8e823971923042d2d442e1
Eli <elis.vlastovka@gmail.com>
2019-05-10 19:16:04 +0200

    juch

--- fonts.py
+++ fonts.py
@@ -8,4 +8,8 @@ from .quality_constants import ANTIALIASING, RESOLUTION_DPI
 
 @functools.lru_cache(None)
 def get_font(size, font=FONT):
-    return ImageFont.truetype(font, size=int(size * RESOLUTION_DPI * ANTIALIASING / 100))
+    try:
+        return ImageFont.truetype(font, size=size * RESOLUTION_DPI * ANTIALIASING // 100)
+    except OSError:
+        return ImageFont.truetype(FONT, size=size * RESOLUTION_DPI * ANTIALIASING // 100)
+
